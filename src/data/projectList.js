65bc2406683a536775b690baa213081c22d1e19e
bepyan <rlantia97@kumoh.ac.kr>
2021-04-02 14:25:45 +0900

    split data

---
+++ src/data/projectList.js
@@ -0,0 +1,90 @@
+export const projectList = [
+    {
+        name: "걸어서 동네 속으로",
+        img: "",
+        period: "2021.03 -",
+        subtitle: "",
+        stack: "ReactNative, TypeScript",
+        discript: "",
+        links: [
+            {
+                name: "깃협",
+                src: "https://github.com/walk-into-town",
+            },
+        ],
+    },
+    {
+        name: "금오공대 책 벼룩시장",
+        img: "",
+        period: "2021.02 - 2020.03",
+        subtitle: "",
+        stack: "Vue, Vuetify",
+        discript: "Vue를 처음 접한 프로젝트입니다. 컴퓨터 공학과 ",
+        links: [
+            {
+                name: "깃협",
+                src: "https://github.com/bepyan/kit-fleabook",
+            },
+        ],
+    },
+    {
+        name: "팔이피플",
+        img: "",
+        period: "2020.09 - 2020.11",
+        subtitle: "",
+        stack: "React",
+        discript: "React를 처음 접하면서 진행한 프로젝트입니다.",
+        links: [
+            {
+                name: "사이트",
+                src: "https://bepyan.github.io/fleamarket-82/"
+            },
+            {
+                name: "깃협",
+                src: "https://github.com/bepyan/fleamarket-82",
+            },
+        ],
+    },
+    {
+        name: "BlablaWeb (횡설수설)",
+        img: "",
+        period: "2020.01 - 2020.02",
+        subtitle: "",
+        stack: "python, ",
+        discript: "",
+        links: [
+            {
+                name: "깃협",
+                src: "https://github.com/bepyan/BlablaWeb",
+            },
+        ],
+    },
+    {
+        name: "Forumate",
+        img: "",
+        period: "2019.09 - 2019.11",
+        subtitle: "",
+        stack: "Java, JavaFX, Socket, Protocol, MySQL",
+        discript: "",
+        links: [
+            {
+                name: "깃협",
+                src: "https://github.com/Foumate/Foumate",
+            },
+        ],
+    },
+    {
+        name: "KIT 기숙사 관리 프로그램",
+        img: "",
+        period: "2019.03 - 2019.06",
+        subtitle: "",
+        stack: "Java, JavaFX, Socket, Protocol, MySQL",
+        discript: "",
+        links: [
+            {
+                name: "깃협",
+                src: "https://github.com/kit-dormitorty-manger",
+            },
+        ],
+    },
+]
---
+++ src/data/stackList.js
@@ -0,0 +1,42 @@
+
+export const stackList = [
+    {
+        title: "Language",
+        list: [
+            { stack: "C++", rate: 4 },
+            { stack: "JAVA", rate: 4 },
+            { stack: "Python", rate: 2 },
+            { stack: "JavaScript(ES6)", rate: 5 },
+            { stack: "TypeScript", rate: 3 },
+        ]
+    },
+    {
+        title: "Front-end",
+        list: [
+            { stack: "HTML/CSS/SCSS", rate: 4 },
+            { stack: "VanillaJS", rate: 4 },
+            { stack: "React", rate: 5 },
+            { stack: "Vue", rate: 3 },
+            { stack: "Django", rate: 1 },
+            { stack: "ReactNative", rate: 5 },
+        ]
+    },
+    {
+        title: "Back-end",
+        list: [
+            { stack: "NodeJS(Express)", rate: 1 },
+            { stack: "MySQL", rate: 1 },
+            { stack: "Mongoose", rate: 1 },
+            { stack: "Firebase", rate: 1 },
+            { stack: "AWS", rate: 1 },
+        ]
+    },
+    {
+        title: "else",
+        list: [
+            { stack: "Crawling", rate: 1 },
+            { stack: "NLP", rate: 1 },
+            { stack: "GTP-2", rate: 1 },
+        ]
+    }
+]
