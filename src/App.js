7c42164687fdcf00e6cbdea09f86c2474a0f2b6f
bepyan <rlantia97@kumoh.ac.kr>
2021-04-02 16:28:39 +0900

    deploy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from 'container/Home';
 
 const App = () => {
     return (
-        <Router>
-            <Switch>
-                <Route exact path='/' component={Home}/>
-            </Switch>
-        </Router>
+        <Home />
     );
 }
 
