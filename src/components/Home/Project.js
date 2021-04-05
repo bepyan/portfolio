b56a2b7829b7c65b7d3e2b885b46a741f9e9365c
bepyan <rlantia97@kumoh.ac.kr>
2021-04-05 18:01:19 +0900

    Update Project img detail

--- src/components/Home/Project.js
+++ src/components/Home/Project.js
@@ -2,14 +2,16 @@ import React from 'react'
 import { SubTitle, Title } from 'style'
 import { projectList } from 'data/projectList'
 import styled from 'styled-components'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 const Project = () => {
 
-    const renderLinks = project => project.links.map((link,idx) => {
+    const renderLinks = project => project.links.map((link, idx) => {
 
-        return <span key={link.src} >
+        return <span key={link.src}>
             {idx > 0 ? <span>, </span> : null}
-            <a href={link.src}>
+            <a href={link.src} target={link.src !== "javascript:void(0);" ? "_blank" : ""}>
                 {link.name}
             </a>
         </span>
@@ -19,14 +21,29 @@ const Project = () => {
         <div>
             <Title>프로젝트</Title>
             {projectList.map((item, idx) => <ProjectWrapper key={idx}>
-                <SubTitle>{item.name}</SubTitle>
-                <ul>
+                <SubTitle style={{fontWeight: 700}}>{item.name}</SubTitle>
+
+
+                <Carousel showThumbs={item.imgs.length > 1} showArrows={false} showIndicators={false} showStatus={false}>
+
+                    {item.imgs.map(img => <div>
+                            <img src={img} alt={img}/>
+                        </div>
+                    )}
+
+                </Carousel>
+
+                <LiWrapper>
                     <li>{item.period}</li>
+                    <li>개발인원: {item.devMembers}</li>
                     <li>{item.subtitle}</li>
                     <li>{item.stack}</li>
-                    <li>{item.discript}</li>
-                    {renderLinks(item)}
-                </ul>
+                    <li>{item.discript.map(e => <div>{e}</div>)}</li>
+                    <LinksWrapper>
+                        {renderLinks(item)}
+                    </LinksWrapper>
+                    <HRLine />
+                </LiWrapper>
             </ProjectWrapper>)}
         </div>
     )
@@ -35,5 +52,17 @@ const Project = () => {
 const ProjectWrapper = styled.div`
     padding: 1rem 0;
 `
-
+const LiWrapper = styled.ul`
+    & > li {
+        margin-top: 4px;
+    }
+`
+const HRLine = styled.hr`
+    margin-top: 3rem;
+    border-top: 1px dashed #bbb;
+    border-bottom: 1px dashed #fff;
+`
+const LinksWrapper = styled.div`
+    margin-top: 1rem;
+`
 export default Project
--- src/data/projectList.js
+++ src/data/projectList.js
@@ -1,92 +1,148 @@
 export const projectList = [
     {
         name: "걸어서 동네 속으로",
-        img: "",
+        imgs: [],
         period: "2021.03 -",
-        subtitle: "",
-        stack: "ReactNative, TypeScript",
-        discript: "",
+        devMembers: "4",
+        subtitle: "포켓몬GO + SNS + 마켓팅",
+        stack: "ReactNative, TypeScript, Expo, AWS, Express(TS), React, Python, GAN, GTP-3",
+        discript: [],
         links: [
             {
-                name: "깃협",
+                name: "깃허브",
                 src: "https://github.com/walk-into-town",
             },
+            {
+                name: "노션(개발로드맵)",
+                src: "https://www.notion.so/bepyan/2d9220f8ef384fbeb008bbe55016c591?v=ae4a1f026289470d8a40f6323a7985c7"
+            }
         ],
     },
     {
         name: "금오공대 책 벼룩시장",
-        img: "",
-        period: "2021.02 - 2020.03",
-        subtitle: "",
-        stack: "Vue, Vuetify",
-        discript: "Vue를 처음 접한 프로젝트입니다. 컴퓨터 공학과 ",
+        imgs: [
+            "https://user-images.githubusercontent.com/65283190/113546655-5e9a6600-9627-11eb-882c-e1f13efb817f.png"
+        ],
+        period: "2021.02 - 2021.03",
+        devMembers: "3 (8) 총 프로젝트에는 8명 참여",
+        subtitle: "벼룩시장 책 예약 사이트",
+        stack: "Vue, Vuetify, Firebase",
+        discript: [
+            "Vue를 처음 접한 프로젝트입니다. 총학생회 학생복지위원회와 연계하여 진행했습니다.",
+            "사용자가 핸드폰에서도 쉽게 사용할 수 있도록 간결하게 반응형 웹으로 만들었습니다.",
+            "3주 안으로 빠르게 만들어야 했기에 디자인 프레임워크 Vuetify을 사용했습니다. 전에 React로 개발한 경험이 있어서 Vue를 빠르게 습득할 수 있었습니다."
+        ],
         links: [
             {
                 name: "사이트",
                 src: "https://kit-fleamarket.web.app/about"
             },
             {
-                name: "깃협",
+                name: "깃허브",
                 src: "https://github.com/bepyan/kit-fleabook",
             },
+            {
+                name: "결과보고",
+                src: "https://www.kumoh.ac.kr/ko/sub06_01_01_02.do?mode=view&articleNo=303990&article.offset=0&articleLimit=10&srSearchVal=%EC%B1%85%EB%B2%BC%EB%A3%A9%EC%8B%9C%EC%9E%A5&srSearchKey=article_title"
+            }
         ],
     },
     {
         name: "팔이피플",
-        img: "",
+        imgs: [
+            "https://user-images.githubusercontent.com/65283190/113546771-9bfef380-9627-11eb-9afb-1a4afa6d1d46.png",
+            "https://user-images.githubusercontent.com/65283190/113547001-1596e180-9628-11eb-8219-8cffe7e8a6e2.png",
+        ],
         period: "2020.09 - 2020.11",
-        subtitle: "",
-        stack: "React",
-        discript: "React를 처음 접하면서 진행한 프로젝트입니다.",
+        devMembers: "4",
+        subtitle: "스마트 컨트랙트를 통한 안전한 중고거래 플랫폼",
+        stack: "React, MySQL, Firebase, BlockChain(Ethereum, Metamask, Smart Contract)",
+        discript: [
+            "React를 처음 접하면서 진행한 프로젝트입니다. 빠르게 UI를 구축하기 위해 Material-ui를 사용했습니다.",
+            "JavaScript도 처음 접했던 프로젝트여서 비동기식 처리에 고생했습니다.",
+            "메타마스크(Metamask) 크롬 플로그인을 활용하여 쉽게 이더리움으로 거래할 수 있습니다."
+        ],
         links: [
             {
                 name: "사이트",
                 src: "https://bepyan.github.io/fleamarket-82/"
             },
             {
-                name: "깃협",
+                name: "깃허브",
                 src: "https://github.com/bepyan/fleamarket-82",
             },
         ],
     },
     {
-        name: "BlablaWeb (횡설수설)",
-        img: "",
-        period: "2020.01 - 2020.02",
-        subtitle: "",
-        stack: "python, ",
-        discript: "",
+        name: "Forumate",
+        imgs: [
+            "https://user-images.githubusercontent.com/65283190/113538218-9ef0e880-9615-11eb-821e-c136c2298edd.png",
+            "https://user-images.githubusercontent.com/65283190/113538338-f2fbcd00-9615-11eb-8fdb-d6664a98b9e6.png",
+            "https://user-images.githubusercontent.com/65283190/113538380-09a22400-9616-11eb-8278-f23d5a9f6913.png",
+            "https://user-images.githubusercontent.com/65283190/113538275-c47df200-9615-11eb-929d-4f756651e5fa.png",
+            "https://user-images.githubusercontent.com/65283190/113538298-d2337780-9615-11eb-9aea-da0691b5adf8.png",
+        ],
+        period: "2020.05 - 2020.06",
+        devMembers: "2",
+        subtitle: "공공시설 조회, 여러 단체의 일정 관리, 단체 통합 SNS 시스템",
+        stack: "Java, JavaFX, Network, Protocol, MySQL, CSS",
+        discript: [
+            "창의프로젝트 과목의 결과물입니다.",
+            "공공데이터포털에서 전국 공공시설 개방정보 표준데이터를 통해 실제 예약이 가능한 공공시설 정보를 제공합니다.",
+            "사용자가 여러 단체에 소속되어 다양한 일정을 한눈에 볼 수 있으며 각 단체의 피드를 통합하여 볼 수 있습니다.",
+            "인스타그램을 벤치마킹하여 핸드폰 화면을 전제로 UI를 설계하였습니다.",
+            "직접 캘린더 GUI를 구현했던 것이 기억에 남습니다."
+        ],
         links: [
             {
-                name: "깃협",
+                name: "깃허브",
+                src: "https://github.com/Foumate/Forumate",
+            },
+        ],
+    }, 
+    {
+        name: "BlablaWeb (횡설수설)",
+        imgs: [
+            "https://user-images.githubusercontent.com/65283190/113548216-1df01c00-962a-11eb-9e8e-1754776152d9.png"
+        ],
+        period: "2020.01 - 2020.02",
+        devMembers: "3",
+        subtitle: "작문 및 글쓰기 보조 사이트",
+        stack: "Python, Django, Crawing, NPL(형태소분석), Word2Vec, GTP-2",
+        discript: [
+            "문장을 분석하여 수식어구를 추천하며 추가로 작문해줍니다.",
+            "국립국어원 한국어기초사전, 한국문화예술위원회 사이버문학광장을 크롤링하여 데이터셋을 추출했습니다.",
+            `Komoran을 통해 형태소 분석하여 어미를 추출하고 Word2Vec을 통해 단어간 유사도를 분석하여 수식언 데이터셋을 만듭니다.
+                구문분석기는 한국전자통신연구원의 ETRI API를 사용하여 에서 수식어가 붙지 않은 명사를 추출하여 데이터셋에서 해당되는 수식언을 추천해줍니다.`,
+            "또한 기존 데이터셋을 학습한 GTP-2 모델을 통해 기존 글 바탕으로 한 문장씩 문장을 생성합니다."
+        ],
+        links: [
+            {
+                name: "깃허브",
                 src: "https://github.com/bepyan/BlablaWeb",
             },
-        ],
-    },
-    {
-        name: "Forumate",
-        img: "",
-        period: "2019.09 - 2019.11",
-        subtitle: "",
-        stack: "Java, JavaFX, Socket, Protocol, MySQL",
-        discript: "",
-        links: [
             {
-                name: "깃협",
-                src: "https://github.com/Foumate/Foumate",
+                name: "사이트(추후 배포 예정)",
+                src: "javascript:void(0);",
             },
         ],
     },
     {
-        name: "KIT 기숙사 관리 프로그램",
-        img: "",
-        period: "2019.03 - 2019.06",
-        subtitle: "",
-        stack: "Java, JavaFX, Socket, Protocol, MySQL",
-        discript: "",
+        name: "KIT 기숙사 관리 시스템",
+        imgs: ["https://user-images.githubusercontent.com/65283190/113542750-e1b7be00-961f-11eb-8e63-1ccf7d57438b.png",
+            "https://user-images.githubusercontent.com/65283190/113544670-b3d47880-9623-11eb-8e37-8e36469af616.png"],
+        period: "2019.09 - 2019.11",
+        devMembers: "6",
+        subtitle: "금오공대 기숙사 관리 시스템",
+        stack: "Java, JavaFX, Network, Protocol, MySQL, CSS",
+        discript: [
+            "융합프로젝트 과목의 결과물입니다. 프론트, 벡엔드, 네트워크 2명씩 팀으로 구성했으며 저는 프론트를 맡았습니다.",
+            "MVC패턴으로 시스템 디랙토리 구조를 설계하려 노력했습니다.",
+            "JavaFX에서 제공하는 UI기능이 빈약하여 많은 구글링과 CSS를 활용하여 만족스러운 결과물이 나왔습니다."
+        ],
         links: [
             {
-                name: "깃협",
+                name: "깃허브",
                 src: "https://github.com/kit-dormitorty-manger",
             },
         ],
