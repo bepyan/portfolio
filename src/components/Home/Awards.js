d1bb53961b7d2b9861f8c5091986645f50b4c5b0
bepyan <rlantia97@kumoh.ac.kr>
2021-04-05 15:06:52 +0900

    update Awards detail

--- src/components/Home/Awards.js
+++ src/components/Home/Awards.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import { Title } from 'style'
+import styled from 'styled-components'
 
 const Awards = () => {
     return (
         <div>
             <Title>수상경력</Title>
-            <p>창의문제해결능력 경진대회 은상</p>
-            <p>중국어 말하기 대회 장려상</p>
-            <p>KIT 캡스톤 장력상</p>
-            <p>캠퍼스 사진 공모전 최우수상</p>
+            <ListWrapper>
+                <p>제10회 전국 창의문제 해결능력 경진대회 은상</p>
+                <p>주부산중국총영사관 주최 대학생중국어말하기대회 장려상</p>
+                <p>KIT Engineering Fair 2020 일반 캡스톤디자인 장려상</p>
+                <p>KIT 캠퍼스 사진 공모전 최우수상</p>
+            </ListWrapper>
         </div>
     )
 }
+
+const ListWrapper = styled.div`
+    & > * {
+        margin: 1rem 0;
+        font-size: 16px;
+    }
+`
 
 export default Awards
