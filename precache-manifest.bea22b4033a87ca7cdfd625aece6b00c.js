f98dbb82a9909d680bb3f7b8a5fe73e1af465f17
bepyan <rlantia97@kumoh.ac.kr>
2021-04-06 22:19:09 +0900

    Updates

---
+++ precache-manifest.bea22b4033a87ca7cdfd625aece6b00c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1302ba4a1348788f1cfa7db401fb1b6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "719474b806a797b4f0ac",
+    "url": "/portfolio/static/css/2.3fc42fd8.chunk.css"
+  },
+  {
+    "revision": "1dc6a4699b5e5333e669",
+    "url": "/portfolio/static/css/main.383bf01a.chunk.css"
+  },
+  {
+    "revision": "719474b806a797b4f0ac",
+    "url": "/portfolio/static/js/2.0bfcb858.chunk.js"
+  },
+  {
+    "revision": "522e03d803bfdb0e0cda4e24f58dcf37",
+    "url": "/portfolio/static/js/2.0bfcb858.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dc6a4699b5e5333e669",
+    "url": "/portfolio/static/js/main.dc040a75.chunk.js"
+  },
+  {
+    "revision": "3d1535822e94bfb1bbd0",
+    "url": "/portfolio/static/js/runtime-main.aed5bca8.js"
+  },
+  {
+    "revision": "47fb61abeca56faf941a28a022b31f7d",
+    "url": "/portfolio/static/media/SCDream1.47fb61ab.otf"
+  },
+  {
+    "revision": "b183bdac8472171e3f7a0a428d7600c0",
+    "url": "/portfolio/static/media/SCDream2.b183bdac.otf"
+  },
+  {
+    "revision": "8be5836258dabb5c226e34e53a4c2c37",
+    "url": "/portfolio/static/media/SCDream3.8be58362.otf"
+  },
+  {
+    "revision": "14cc8f9eca7eb20055bf6855b087fa1b",
+    "url": "/portfolio/static/media/SCDream4.14cc8f9e.otf"
+  },
+  {
+    "revision": "86bec888f8bcb9e9e76e433a120c5962",
+    "url": "/portfolio/static/media/SCDream5.86bec888.otf"
+  },
+  {
+    "revision": "9c7a81cb14091c008ab157bf19da2817",
+    "url": "/portfolio/static/media/SCDream6.9c7a81cb.otf"
+  },
+  {
+    "revision": "3bd0c8cad0d8fa056ff66afb70c53b69",
+    "url": "/portfolio/static/media/SCDream7.3bd0c8ca.otf"
+  },
+  {
+    "revision": "d8ebcaf2200242a487e34c4c088f2762",
+    "url": "/portfolio/static/media/SCDream8.d8ebcaf2.otf"
+  },
+  {
+    "revision": "41be699c4a5141c37bc0b940196e3268",
+    "url": "/portfolio/static/media/SCDream9.41be699c.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f1198f49e351491f8d55a385700b6b77.js"
+  "/portfolio/precache-manifest.bea22b4033a87ca7cdfd625aece6b00c.js"
 );
 
 self.addEventListener('message', (event) => {
